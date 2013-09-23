fe90f58e84b003e3915cf79abc700b625f519a4c
Doug <dougjchau@gmail.com>
2013-09-23 14:32:46 -0400

    adds index.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 import os
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 
 def home(request):
-	return HttpResponse("Hello World")
+	return render(request, 'blog/index.html')
+
 
