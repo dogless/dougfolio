6b538b8799eeb1530ecacf8ef54080f7dd6ee315
Doug <dougjchau@gmail.com>
2013-09-23 20:57:23 -0400

    additions to tentative placeholder ui

--- dougfolio/static/custom/js/parallax.js
+++ dougfolio/static/custom/js/parallax.js
@@ -14,14 +14,22 @@ $(document).ready(function(){
 		EasyPeasyParallax();
 	});
 
-	$('#bannertext').hover(
-    function(){
-        $(this).toggleClass('shadow');
-    });
+	$('#sidebar').animate(
+		{marginLeft: "0%", opacity: 0.6}, 300, 'swing'
+	);
+
+	$('#bannertext').animate(
+		{opacity: 1}, 1000, 'linear'
+	);
+
+ // $('#bannertext').hover(
+ //    function(){
+ //        $(this).toggleClass('shadow');
+ //    });
 
 	$("#bannertext").click(function (){
-                $('html, body').animate({
-                    scrollTop: $("#content").offset().top
-				}, 1000, 'swing');
+            $('html, body').animate({
+                scrollTop: $("#content").offset().top
+			}, 1000, 'swing');
     });
 });
